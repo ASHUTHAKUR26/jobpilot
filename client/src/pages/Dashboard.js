474761802f28741e1e22c5c73cd1702057410ec3
Ashu  Kumar Thakur <157728823+ASHUTHAKUR26@users.noreply.github.com>
2026-05-21 15:09:58 +0530

    Fix eslint comment

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -51,8 +51,8 @@ export default function Dashboard() {
     } catch (err) {}
   };
 
-  // eslint-disable-next-line
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+ 
+// eslint-disable-next-line react-hooks/exhaustive-deps
 useEffect(() => {
     fetchJobs();
     fetchStats();
